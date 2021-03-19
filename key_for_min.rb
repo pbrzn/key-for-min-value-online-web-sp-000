aede21b1855af0951b6c94b3032999a3d325291e
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-19 18:18:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,11 @@
 
 def key_for_min_value(name_hash)
   min_value=nil
-  key_for_min_value
+  key_for_min_value=""
   name_hash.each do |key, value|
-    if value>min_value
-      min_value=value
-      
+    if value<min_value
+      key_for_min_value=key
+    end
+  end
+  key_for_min_value
 end
