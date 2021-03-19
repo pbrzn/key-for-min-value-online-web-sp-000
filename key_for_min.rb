d624c8a47261144102f69acc191ef11d95eb2655
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-19 18:25:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,12 @@
 
 def key_for_min_value(name_hash)
   min_value=0
-  min_key=""
+  min_key=nil
   name_hash.each do |key, value|
     if value<min_value
       min_key=key
     end
+    
   end
   min_key
 end
